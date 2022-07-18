355df4f4eed1177ded74bf71b8e88a1dc616be0c
AdnanKhan45 <ak187429@gmail.com>
2022-07-19 01:06:53 +0500

    update

--- lib/data/models/user_model.dart
+++ lib/data/models/user_model.dart
@@ -2,10 +2,11 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class UserModel {
+  final String? id;
   final String? username;
   final String? age;
 
-  UserModel({this.username, this.age});
+  UserModel({this.username, this.age, this.id});
 
   factory UserModel.fromSnapshot(DocumentSnapshot snap) {
     var snapshot = snap.data() as Map<String, dynamic>;
@@ -13,11 +14,13 @@ class UserModel {
     return UserModel(
       username: snapshot['username'],
       age: snapshot['age'],
+      id: snapshot['id'],
     );
   }
 
   Map<String, dynamic> toJson() => {
     "username": username,
     "age": age,
+    "id": id,
   };
 }
